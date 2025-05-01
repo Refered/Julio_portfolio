5b4772b1d0fd4f5263bbebe2d8746cbd85ea3c0d
unknown <alphaceves@gmail.com>
2025-05-01 13:19:09 -0600

    finished nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,38 @@ import Skills from "./Sections/Skills/Skills";
 import Portfolio from "./Sections/Portfolio/Portfolio";
 import Contact from "./Sections/Contact/Contact";
 
+import { ReactComponent as NavOpen } from "./Icons/NavOpen.svg";
+
 function App() {
   const [currentSection, setCurrentSection] = useState(0);
+  const [navOpen, setNavOpen] = useState(false);
 
   return (
     <div className="app">
-      <NavBar setCurrentSection={setCurrentSection} />
-      <div className="section_carrousel">
-        <Home sectionState={currentSection == 0 ? "active" : "inactive"} />
-        <About sectionState={currentSection == 1 ? "active" : "inactive"} />
-        <Skills sectionState={currentSection == 2 ? "active" : "inactive"} />
-        <Portfolio sectionState={currentSection == 3 ? "active" : "inactive"} />
-        <Contact sectionState={currentSection == 4 ? "active" : "inactive"} />
+      <NavBar
+        currentSection={currentSection}
+        setCurrentSection={setCurrentSection}
+        navOpen={navOpen}
+        setNavOpen={setNavOpen}
+      />
+      <div
+        className="section_carrousel"
+        onClick={(e) => {
+          if (!e.target.closest(".nav_open")) {
+            setNavOpen(false);
+          }
+        }}
+      >
+        <div className="nav_open" onClick={() => setNavOpen(true)}>
+          <NavOpen />
+        </div>
+        <Home sectionState={currentSection === 0 ? "active" : "inactive"} />
+        <About sectionState={currentSection === 1 ? "active" : "inactive"} />
+        <Skills sectionState={currentSection === 2 ? "active" : "inactive"} />
+        <Portfolio
+          sectionState={currentSection === 3 ? "active" : "inactive"}
+        />
+        <Contact sectionState={currentSection === 4 ? "active" : "inactive"} />
       </div>
     </div>
   );
--- src/Sections/NavBar/NavBar.jsx
+++ src/Sections/NavBar/NavBar.jsx
@@ -6,31 +6,47 @@ import { ReactComponent as AboutIcon } from "../../Icons/UserCircle.svg";
 import { ReactComponent as SkillsIcon } from "../../Icons/LineSegments.svg";
 import { ReactComponent as PortfolioIcon } from "../../Icons/Briefcase.svg";
 import { ReactComponent as ContactIcon } from "../../Icons/ChatsCircle.svg";
+import { ReactComponent as NavClose } from "../../Icons/NavClose.svg";
 
-const NavBar = ({ setCurrentSection }) => {
+const NavBar = ({ currentSection, setCurrentSection, navOpen, setNavOpen }) => {
   return (
-    <nav>
+    <nav className={navOpen ? "active" : "inactive"}>
       <div className="logo_container">Julio Portfolio</div>
       <div className="nav_list">
-        <div className="list_element">
-          <HomeIcon viewBox="0 0 32 32" />
-          <p onClick={() => setCurrentSection(0)}>Home</p>
+        <div className="list_element" onClick={() => setCurrentSection(0)}>
+          <HomeIcon
+            viewBox="0 0 32 32"
+            className={currentSection === 0 ? "" : "unselected"}
+          />
+          <p>Home</p>
         </div>
-        <div className="list_element">
-          <AboutIcon viewBox="0 0 32 32" />
-          <p onClick={() => setCurrentSection(1)}>About</p>
+        <div className="list_element" onClick={() => setCurrentSection(1)}>
+          <AboutIcon
+            viewBox="0 0 32 32"
+            className={currentSection === 1 ? "" : "unselected"}
+          />
+          <p>About</p>
         </div>
-        <div className="list_element">
-          <SkillsIcon viewBox="0 0 32 32" />
-          <p onClick={() => setCurrentSection(2)}>Skills</p>
+        <div className="list_element" onClick={() => setCurrentSection(2)}>
+          <SkillsIcon
+            viewBox="0 0 32 32"
+            className={currentSection === 2 ? "" : "unselected"}
+          />
+          <p>Skills</p>
         </div>
-        <div className="list_element">
-          <PortfolioIcon viewBox="0 0 32 32" />
-          <p onClick={() => setCurrentSection(3)}>Portfolio</p>
+        <div className="list_element" onClick={() => setCurrentSection(3)}>
+          <PortfolioIcon
+            viewBox="0 0 32 32"
+            className={currentSection === 3 ? "" : "unselected"}
+          />
+          <p>Portfolio</p>
         </div>
-        <div className="list_element">
-          <ContactIcon viewBox="0 0 32 32" />
-          <p onClick={() => setCurrentSection(4)}>Contact</p>
+        <div className="list_element" onClick={() => setCurrentSection(4)}>
+          <ContactIcon
+            viewBox="0 0 32 32"
+            className={currentSection === 4 ? "" : "unselected"}
+          />
+          <p>Contact</p>
         </div>
       </div>
     </nav>
